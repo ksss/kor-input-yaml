72474338f60fbaaa19312c04a2c65975423a5e48
ksss <co000ri@gmail.com>
2015-10-11 13:31:36 +0900

    Fix select keys order

--- lib/kor/input/yaml.rb
+++ lib/kor/input/yaml.rb
@@ -9,6 +9,7 @@ module Kor
         @yamls = []
         @prekeys = nil
         @count = 0
+        @stock = []
         @fiber = Fiber.new do
           @yamls.each do |yaml|
             Fiber.yield @keys.map{ |k| yaml[k] }
@@ -19,17 +20,13 @@ module Kor
       end
 
       def parse(opt)
-        opt.on("--key=KEY", "preset keys (e.g. foo,bar,baz)") do |arg|
+        opt.on("--key=KEY", "select keys (e.g. foo,bar,baz)") do |arg|
           @prekeys = arg
         end
       end
 
       def head
         if @prekeys
-          line = @io.gets
-          if line.index(START) != 0
-            @io.seek(line.length, IO::SEEK_CUR)
-          end
           @keys = @prekeys.split(",")
         else
           @yamls = YAML.load_stream(@io.read)
@@ -46,17 +43,22 @@ module Kor
 
       def gets
         if @prekeys
-          stock = ""
           while line = @io.gets
-            stock << line
+            @stock << line
             if line.index(START) == 0
-              break
+              if 1 < @stock.length
+                break
+              end
             end
           end
-          if stock == ""
+          if @stock.empty?
             return nil
           end
-          yaml = YAML.load(stock)
+          last_index = line ? -2 : -1
+          yaml = YAML.load(@stock.slice!(0..last_index).join)
+          if @stock.empty?
+            return nil
+          end
           @count += 1
           @keys.map{ |key| yaml[key] }
         else
